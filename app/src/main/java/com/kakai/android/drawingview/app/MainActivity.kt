1cd9c812538bbfc6524962653bfd1f1629ed3c73
Ricardo Carrapiço <r.kakai248@gmail.com>
2019-07-31 16:44:33 +0100

    Initial project structure

---
+++ app/src/main/java/com/kakai/android/drawingview/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kakai.android.drawingview.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
